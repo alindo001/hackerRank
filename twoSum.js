7c92af7ea2d27274501ce83220b60e3030ae21cf
Andres <andreslindo20@gmail.com>
2022-08-14 21:52:50 -0400

    alternative solution

--- twoSum.js
+++ twoSum.js
@@ -36,16 +36,15 @@
  */
  var twoSum = function(nums, target) {
     
-    let map = {}
-    for (i=0;i<nums.length;i++){
-        if(target - nums[i] in map){
-            return [map[target-nums[i]],i]
-        }else{
-            map[nums[i]] = i
+    for(i=0;i<=nums.length;i++){
+        for(j=1;j<=nums.length;j++){
+            if(nums[i]+nums[j]==target&& i!=j){
+                return [i,j]
+            } 
         }
     }
- 
 };
+
 
 console.log(twoSum([2,7,11,15],9))
 console.log(twoSum([3,2,4],6))
